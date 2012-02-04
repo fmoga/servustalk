de683a83f1ad30657e2ec194a4186f5a9d714801
Felix Kerekes <sttwister@gmail.com>
2012-02-04 22:25:23 +0200

    Display history on history page.

--- route.js
+++ route.js
@@ -3,6 +3,7 @@ var requestHandlers = require('./requestHandlers');
 function addRoutes(app) {
     app.get('/', requestHandlers.index);
     app.get('/history', requestHandlers.history);
+    app.post('/getHistory', requestHandlers.getHistory);
 }
 
 exports.addRoutes = addRoutes;
