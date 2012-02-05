f81abc8e3d97878faf61f099f5bc0be678595539
Calin Paul Alexandru <thewickedfreak@gmail.com>
2012-02-05 23:24:30 +0200

    Support for 3 digit color hexas and scroll correcting.

--- public/js/script.js
+++ public/js/script.js
@@ -154,11 +154,12 @@ $(document).ready(function() {
         if (wasScrolledToBottom) scrollToBottom();
       } else if (message.text.indexOf('/#') == 0) {
         var color = message.text.substring(1, message.text.indexOf(' '));
-        if (!color.match(/[a-fA-F0-9]{6}/g)) color = '#AAAAAA';
+        if (!color.match(/[a-fA-F0-9]{6}|[a-fA-F0-9]{3}/g)) color = '#AAAAAA';
         html = '';
         html += '<div style="background: ' + color + '"><b>' + message.user.name + ':<b/> ';
         html += htmlEncode(message.text.substring(message.text.indexOf(' ') + 1))  + ' </div>';
         $('#messagebox .scrollr').append(html);
+        if (wasScrolledToBottom) scrollToBottom();
       }
 
     }
