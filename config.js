f48b5de0b12df09b8836449b544599b31e8aa992
root <root@fmoga-laptop.(none)>
2012-02-21 09:18:31 +0200

    Changed client secret.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var jQuery = require('jquery');
 var config = {}
 
 config.server = {
-    port: 80
+    port: 8000
 }
 
 config.mongo = {
@@ -12,10 +12,10 @@ config.mongo = {
 
 config.app = {
     google_client_id: "596685303616.apps.googleusercontent.com",
-    google_client_secret: "jzmnVYcNNZBAcVjjDe0T-q8k",
+    google_client_secret: "1lHPftfPbfQi-Ft9vJaml2sA",
     history_size: 20,
     sio: {
-        log_level: 1,
+        log_level: 3,
         transports: ['xhr-polling', 'jsonp-polling']
     }
 }
