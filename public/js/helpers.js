b422b5b38f7dede4634c4b785ed0b3c3c9a322a3
Cristian Mircea Messel <mess110@gmail.com>
2012-04-23 00:42:46 +0300

    cleanup helper, now its readable

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,21 +1,35 @@
-addPorfilePic = function(client) {
+addClient = function(client, buddylist, nameStyle) {
+  // vars
   var picture = client.picture ? client.picture : DEFAULT_PICTURE;
   var idle = client.idle ? 'idle' : '';
-  return '<img class="profilepic ' + idle + ' middle" title="' +client.name + '" src="' + picture + '"/>'
-}
 
-addProfileName = function(client, nameStyle) {
+  // profile pic
+  var profilePic = $('<img>');
+  profilePic.addClass('profilepic');
+  profilePic.addClass(idle);
+  profilePic.addClass('middle');
+  profilePic.attr('title', client.name);
+  profilePic.attr('src', picture);
+
+  // profile name
+  var profileName = $('<span>');
+  profileName.addClass('profilename');
+  profileName.addClass(idle);
+  profileName.addClass(nameStyle);
+  profileName.html(client.name);
+
+  // idle since
   var idleSpan = $('<span>');
-  idleSpan.addClass('idleSince');
+  idleSpan.addClass('idleSpan');
   idleSpan.attr('idleSince', (new Date().getTime() - client.idleFor));
-  
-  var idleSince = client.idle ? idleSpan.html() : '';
-  pName = $('<span>');
-  pName.addClass('idleSince').addClass(nameStyle).html(client.name);
-  return pName.html() + idleSince;
-}
+  idleSpan = '<span class="idleSince" idleSince="' + (new Date().getTime() - client.idleFor) + '"></span>';
+  var idleSince = client.idle ? idleSpan : '';
 
-addClient = function(client, buddylist, nameStyle) {
-  clientLi = $("<li>").html(addPorfilePic(client) + addProfileName(client, nameStyle));
-  $(buddylist).html(clientLi);
+  // the li
+  var li = $('<li>');
+  li.append(profilePic);
+  li.append(profileName);
+  li.append(idleSince);
+
+  buddylist.append(li);
 }
