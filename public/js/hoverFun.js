d12a24772c2879b5cb4a674e1dd90b5f32693ead
florian <florian@mail.ubuntzeii.cloudns.org>
2012-05-25 09:38:07 +0300

    Removed "fun" hover over login button

--- public/js/hoverFun.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#hoverFun').css('position', 'absolute');
-  $('#hoverFun').on('hover', function() {
-    console.log(this);
-    $('#hoverFun').css('top', Math.floor(Math.random() * 200));
-    $('#hoverFun').css('left', Math.floor(Math.random() * 200));
-  });
-});
-
