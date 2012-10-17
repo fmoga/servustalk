a15da93e6c6ce376a67b6be54aed56785577cdbd
Brisc Bogdan <sirbrisc+github@gmail.com>
2012-10-18 02:35:03 +0300

    Dark theme

--- public/js/ui.js
+++ public/js/ui.js
@@ -32,4 +32,48 @@ $(document).ready(function () {
   $(".loading").removeClass('loading');
   $(".preloader").removeClass('preloader');
   resize();
+    UI.init();
 });
+//UI
+var Utils = {
+    store: function( namespace, data ) {
+        if ( arguments.length > 1 ) {
+            return localStorage.setItem( namespace, JSON.stringify( data ) );
+        } else {
+            var store = localStorage.getItem( namespace );
+            return ( store && JSON.parse( store ) ) || [];
+        }
+    }
+};
+
+var UI = {
+    init:function(){
+        this.theme= Utils.store("theme")|| "";
+        if(this.theme=="")this.theme="custom";
+
+        this.cacheElements();
+        this.bindEvents()
+        this.updateTheme();
+    },
+    cacheElements:function(){
+        this.$theme_link=$("#theme_link");
+        console.log(this.$theme_link);
+
+    },
+    bindEvents:function(){
+        $("#themeLight").click(function(){
+            UI.setTheme("custom");
+        })
+        $("#themeDark").click(function(){
+            UI.setTheme("custom_dark");
+        })
+    },
+    setTheme:function(theme){
+        this.theme=theme;
+        Utils.store("theme",theme);
+        UI.updateTheme();
+    },
+    updateTheme:function(){
+        this.$theme_link.attr("href","/public/css/"+this.theme+".css");
+    }
+}
